c3b42c415f9c2796b74a07214cc1f2f4d335e837
Zac Leids <zacleids@aol.com>
2015-03-14 15:37:49 -0700

    added passport and card dependice classes

--- app/src/main/java/edu/csumb/itcd/ideasofmarch2015/Passport.java
+++ app/src/main/java/edu/csumb/itcd/ideasofmarch2015/Passport.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
+import org.jmrtd.PassportService;
+import net.sf.scuba.smartcards.CardService;
+import net.sf.scuba.smartcards.CardTerminalListener;
+import net.sf.scuba.smartcards.CardFileInputStream;
 
 public class Passport extends ActionBarActivity {
 
@@ -19,17 +22,20 @@ public class Passport extends ActionBarActivity {
     /*
     public void ScanPass(){
     boolean success = false;
-    if(success == true)
-    {
+    if(success == true){
         Intent intent = new Intent(this, Passport_Info.class);
         startActivity(intent);
     }
-    else
-    {
+    else{
+        CardService service = new CardFileInputStream();
+        PassportService pass = new PassportService(service);
+        short var = 0;
+        pass.getInputStream(var);
+    }
     //Scan again
     //Keep scanning
     }
-     */
+    */
 
     public void F_BackP(View view){
         Intent intent = new Intent(this, ScanDoc.class);
