7851e45d7899a22cfec30e4813cb84fd617a9f5f
rgreen1207 <djrain>
2015-03-14 22:58:34 -0700

    Android Studio is Sentient!!!

--- app/src/main/java/edu/csumb/itcd/ideasofmarch2015/ScanCode.java
+++ app/src/main/java/edu/csumb/itcd/ideasofmarch2015/ScanCode.java
@@ -6,8 +6,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.
-
 
 
 public class ScanCode extends ActionBarActivity {
