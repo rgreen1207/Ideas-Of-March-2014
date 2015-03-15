c4196d762e14187c7784700836b30593e352b49f
Greg Greenleaf <pacohojaverde@gmail.com>
2015-03-15 02:35:35 -0700

    Tesseract API files added. Needs restructuring to work.

--- app/src/main/java/edu/csumb/itcd/ideasofmarch2015/MainActivity.java
+++ app/src/main/java/edu/csumb/itcd/ideasofmarch2015/MainActivity.java
@@ -1,11 +1,22 @@
 package edu.csumb.itcd.ideasofmarch2015;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -16,14 +27,12 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-
-
     public void F_ScanDoc(View view){
         Intent intent = new Intent(this, ScanDoc.class);
         startActivity(intent);
     }
 
-    public void F_ScanCode(){
+    public void F_ScanCode() {
         Intent intent = new Intent(this, ScanCode.class);
         startActivity(intent);
     }
@@ -54,4 +63,77 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void ocr(View v) {
+
+        String _path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/DCIM/Camera/" + "filename.jpg";
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 2;
+        Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
+
+        try {
+            ExifInterface exif = new ExifInterface(_path);
+            int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+            //Log.v(LOG_TAG, "Orient: " + exifOrientation);
+
+            int rotate = 0;
+            switch (exifOrientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotate = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotate = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotate = 270;
+                    break;
+            }
+
+            //Log.v(LOG_TAG, "Rotation: " + rotate);
+
+            if (rotate != 0) {
+
+                // Getting width & height of the given image.
+                int w = bitmap.getWidth();
+                int h = bitmap.getHeight();
+
+                // Setting pre rotate
+                Matrix mtx = new Matrix();
+                mtx.preRotate(rotate);
+
+                // Rotating Bitmap
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+                // tesseract req. ARGB_8888
+                bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+            }
+
+        } catch (IOException e) {
+            //Log.e(LOG_TAG, "Rotate or coversion failed: " + e.toString());
+        }
+
+        ImageView iv = (ImageView) findViewById(R.id.image);
+        iv.setImageBitmap(bitmap);
+        iv.setVisibility(View.VISIBLE);
+
+        //Log.v(LOG_TAG, "Before baseApi");
+
+        TessBaseAPI baseApi = new TessBaseAPI();
+        baseApi.setDebug(true);
+        baseApi.init("ideasOfMarch2015\\app\\src\\main\\assets", "eng");
+        baseApi.setImage(bitmap);
+        String recognizedText = baseApi.getUTF8Text();
+        baseApi.end();
+
+        //Log.v(LOG_TAG, "OCR Result: " + recognizedText);
+
+        // clean up and show
+        //if (LANG.equalsIgnoreCase("eng")) {
+        recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
+        //}
+        if (recognizedText.length() != 0) {
+            ((TextView) findViewById(R.id.field)).setText(recognizedText.trim());
+        }
+    }
+
 }
